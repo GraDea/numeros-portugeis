13aaecf74cd6033380719a4c415a79aa4a93584d
Evgeniy Peshkov <e.peshkov@dodopizza.com>
2022-12-03 19:36:33 +0000

    new logo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import './App.css';
 import writtenNumber from 'written-number';
+import { useState } from 'react';
+
+
 
 function App() {
-  let numero = Math.floor(Math.random() * 30_000);
+
+  const [numero, setNumero] = useState({n: Math.floor(Math.random() * 30_000), v: false})
   writtenNumber.defaults.lang = "ptPT";
-  let text = writtenNumber(numero)
+  
+
+  const renewNumero = ()=>{
+    if(numero.v)
+    {
+      setNumero( {n: Math.floor(Math.random() * 30_000), v: false} );
+    }
+    else
+    {
+      setNumero( {n: numero.n, v: true})
+    }
+
+    
+  }
+  
   return (
-    <div className="App">
+    <div className="App" onClick={renewNumero}>
       <header className="App-header">
         <p>
           Estudar dos nomeros
         </p>
         <p>
-        {numero} - {text}
+        {numero.n}
       </p>
+      <p style={{visibility: numero.v ? 'visible' : 'hidden'}}>{writtenNumber(numero.n)}</p>
       </header>
     </div>
   );
